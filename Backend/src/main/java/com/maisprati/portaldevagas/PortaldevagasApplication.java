6f16de8c6f27fe6f95379f9680e932a3f1bb9b84
Gilbertoacl <betoacl@hotmail.com>
2025-08-08 16:38:02 -0300

    feat: Adicionado os arquivos do projeto backend.

---
+++ Backend/src/main/java/com/maisprati/portaldevagas/PortaldevagasApplication.java
@@ -0,0 +1,13 @@
+package com.maisprati.portaldevagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortaldevagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortaldevagasApplication.class, args);
+	}
+
+}
