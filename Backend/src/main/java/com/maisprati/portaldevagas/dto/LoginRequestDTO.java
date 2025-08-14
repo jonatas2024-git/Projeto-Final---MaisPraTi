66ca9f4bfb9de7b3872858c09ce51e3921af95dd
Gilbertoacl <betoacl@hotmail.com>
2025-08-14 19:12:04 -0300

    refactor: Atualizado a validação dos dtos de request e response do endpoit de login

--- Backend/src/main/java/com/maisprati/portaldevagas/dto/LoginRequestDTO.java
+++ Backend/src/main/java/com/maisprati/portaldevagas/dto/LoginRequestDTO.java
@@ -1,3 +1,8 @@
 package com.maisprati.portaldevagas.dto;
 
-public record LoginRequestDTO ( String login, String password) {}
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequestDTO (
+        @NotBlank(message = "Login é obrigatório") String login,
+        @NotBlank(message = "Password é obrigatório") String password
+) {}
--- Backend/src/main/java/com/maisprati/portaldevagas/dto/ResponseDTO.java
+++ Backend/src/main/java/com/maisprati/portaldevagas/dto/ResponseDTO.java
@@ -2,5 +2,5 @@ package com.maisprati.portaldevagas.dto;
 
 import com.maisprati.portaldevagas.models.user.TypeUser;
 
-public record ResponseDTO(String login, String token, TypeUser typeUser) {
+public record ResponseDTO(String login, TypeUser typeUser, String token) {
 }
