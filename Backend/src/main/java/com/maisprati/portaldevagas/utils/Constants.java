14ae39e66ddd89c8e6aa1361fdc62ede5c3a2a6d
Gilbertoacl <betoacl@hotmail.com>
2025-08-14 19:05:38 -0300

    feat:Adicionado classe de constantes do sistema

---
+++ Backend/src/main/java/com/maisprati/portaldevagas/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.maisprati.portaldevagas.utils;
+
+public class Constants {
+    public static final String INV_CRED = "AUTH_INVALID_CREDENTIALS";
+    public static final String RES_NOT_FOUND = "RESOURCE_NOT_FOUND";
+    public static final String BUSINESS_RULE = "BUSINESS_RULE";
+    public static final String INT_ERROR = "INTERNAL_ERROR";
+    public static final String VALID_ERROR = "VALIDATION_ERROR";
+    public static final String INVALID_REQ = "Request inválido";
+    public static final String SERV_INT_ERROR = "Erro interno no servidor";
+    public static final String AUTH_REQ = "AUTH_REQUIRED";
+    public static final String AUTH_NECESSARIA = "Autenticação necessária";
+    public static final String ACCESS_DENIED = "ACCESS_DENIED";
+    public static final String ACESSO_NEGADO = "Acesso Negado";
+}
