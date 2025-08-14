6f3f924bfd2583013274c090ad4ef1360bde8855
Gilbertoacl <betoacl@hotmail.com>
2025-08-14 19:46:05 -0300

    refactor: Adicionado a validação do request de registro.

--- Backend/src/main/java/com/maisprati/portaldevagas/dto/RegisterRequestDTO.java
+++ Backend/src/main/java/com/maisprati/portaldevagas/dto/RegisterRequestDTO.java
@@ -1,6 +1,11 @@
 package com.maisprati.portaldevagas.dto;
 
 import com.maisprati.portaldevagas.models.user.TypeUser;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record RegisterRequestDTO(String login, String password, TypeUser typeUser) {
+public record RegisterRequestDTO(
+        @NotBlank(message = "Login é obrigatório") String login,
+        @NotBlank(message = "Password é obrigatório") String password,
+        @NotNull(message = "O tipo de pefil é obrigatório") TypeUser typeUser) {
 }
